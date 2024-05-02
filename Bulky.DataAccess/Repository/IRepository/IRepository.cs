a8c97f5fcbca0f4364b1d498db5fac1776a46580
AnaSoftdeveloper <anapaz.softdev@gmail.com>
2024-05-01 21:56:19 -0400

    Implement Repository Interface

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //T-Category
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> predicate); // when we want to write LINQ expression this is the general syntax
+        //FirstOrDefault(u => u.name = name)
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
